c61977db2645e0625cb03d9f4ddc7dfc4f51382f
Catur Krida Cahya Laksana <caturkrida16@gmail.com>
2022-08-01 14:13:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import uuid
 app = Flask(__name__)
 id = uuid.uuid1()
 id_generator = id.hex
-storage_client = storage.Client.from_service_account_json("service_account/service_account-cloud_storage") # This is the credentials
-bucket = storage_client.bucket("kazuyosan-pdf") # Bucket name
+storage_client = storage.Client.from_service_account_json("service_account/service_account-cloud_storage")
+bucket = storage_client.bucket("kazuyosan-pdf")
 
 # API Key
-# api_key = os.environ["API_KEY"] # Call API Key from Environment Variable
+api_key = os.environ["API_KEY"] # Call API Key from Environment Variable
 
 # Routes
 @app.route("/", methods=["GET"])
@@ -24,11 +24,10 @@ def index():
 @app.route("/compress", methods=["POST"])
 def compress():
     # Request file and auth key
-    # auth = request.args.get("auth")
+    auth = request.args.get("auth")
     file = request.files["berkas"]
     
     # Check auth null
-    """
     if auth == "":
         return json.dumps(
             {
@@ -45,53 +44,52 @@ def compress():
         }
         )
     else:
-    """
-    if file:
-        name = file.filename
-        check_extension = name.split(".")
-        
-        # Check extension
-        if check_extension[len(check_extension) - 1] != "pdf":
+        if file:
+            name = file.filename
+            check_extension = name.split(".")
+            
+            # Check extension
+            if check_extension[len(check_extension) - 1] != "pdf":
+                return json.dumps(
+                    {
+                        "status": "Error (File)",
+                        "description": "Wrong file extension. Make sure extension is .pdf"
+                    }
+                    )
+            
+            file.save(name) # Save file in local
+            reader = PdfReader(name, strict=False)
+            writer = PdfWriter()
+            
+            # Compress and write page
+            for page in reader.pages:
+                page.compress_content_streams() # Compress method
+                writer.add_page(page)
+            
+            # Write page to file 
+            with open ("result/compressed-" + id_generator + ".pdf", "wb") as f:
+                writer.write(f)
+            
+            # Uploaded file to GCS
+            blob = bucket.blob("compressed-" + id_generator + ".pdf")
+            blob.upload_from_filename("result/compressed-" + id_generator + ".pdf")
+            
+            # Make public and get link
+            blob.make_public()
+            
+            # Delete all processing file
+            os.remove(name)
+            os.remove("result/compressed-" + id_generator + ".pdf") 
+            
+            return redirect(blob.public_url)
+        else:
             return json.dumps(
                 {
                     "status": "Error (File)",
-                    "description": "Wrong file extension. Make sure extension is .pdf"
+                    "description": "Please add the file"
                 }
                 )
-        
-        file.save(name) # Save file in local
-        reader = PdfReader(name, strict=False)
-        writer = PdfWriter()
-        
-        # Compress and write page
-        for page in reader.pages:
-            page.compress_content_streams() # Compress method
-            writer.add_page(page)
-        
-        # Write page to file 
-        with open ("result/compressed-" + id_generator + ".pdf", "wb") as f:
-            writer.write(f)
-        
-        # Uploaded file to GCS
-        blob = bucket.blob("compressed-" + id_generator + ".pdf")
-        blob.upload_from_filename("result/compressed-" + id_generator + ".pdf")
-        
-        # Make public and get link
-        blob.make_public()
-        
-        # Delete all processing file
-        os.remove(name)
-        os.remove("result/compressed-" + id_generator + ".pdf") 
-        
-        return redirect(blob.public_url)
-    else:
-        return json.dumps(
-            {
-                "status": "Error (File)",
-                "description": "Please add the file"
-            }
-            )
-                
+                    
 @app.route("/merge", methods=["POST"])
 def merge():
     # Request file and auth key
@@ -104,7 +102,6 @@ def merge():
         file[x] = req_file[x]
     
     # Check auth null
-    """
     if auth == "":
         return json.dumps(
             {
@@ -121,55 +118,54 @@ def merge():
         }
         )
     else:
-    """
-    if file:
-        # Check extension
-        for x in file:
-            name = file[x].filename
-            check_extension = name.split(".")
+        if file:
+            # Check extension
+            for x in file:
+                name = file[x].filename
+                check_extension = name.split(".")
+                
+                if check_extension[len(check_extension) - 1] != "pdf":
+                    return json.dumps(
+                        {
+                            "status": "Error (File)",
+                            "description": "Wrong file extension. Make sure extension is .pdf"
+                        }
+                        )
             
-            if check_extension[len(check_extension) - 1] != "pdf":
-                return json.dumps(
-                    {
-                        "status": "Error (File)",
-                        "description": "Wrong file extension. Make sure extension is .pdf"
-                    }
-                    )
-        
-        # Save all file in local
-        for x in file:
-            file[x].save(file[x].filename) 
+            # Save all file in local
+            for x in file:
+                file[x].save(file[x].filename) 
+                
+            merger = PdfMerger(strict=False)
             
-        merger = PdfMerger(strict=False)
-        
-        # Append file to merger
-        for pdf in file:
-            merger.append(file[pdf].filename)
-        
-        # Write append file to merged file
-        merger.write("result/merged-" + id_generator + ".pdf")
-        merger.close()
-        
-        # Uploaded file to GCS
-        blob = bucket.blob("merged-" + id_generator + ".pdf")
-        blob.upload_from_filename("result/merged-" + id_generator + ".pdf")
-        
-        # Make public and get link
-        blob.make_public()
-        
-        # Delete all processing file
-        for x in file:
-            os.remove(file[x].filename)
-        os.remove("result/merged-" + id_generator + ".pdf")
-        
-        return redirect(blob.public_url)
-    else:
-        return json.dumps(
-            {
-                "status": "Error (File)",
-                "description": "Please add the file"
-            }
-            )
+            # Append file to merger
+            for pdf in file:
+                merger.append(file[pdf].filename)
+            
+            # Write append file to merged file
+            merger.write("result/merged-" + id_generator + ".pdf")
+            merger.close()
+            
+            # Uploaded file to GCS
+            blob = bucket.blob("merged-" + id_generator + ".pdf")
+            blob.upload_from_filename("result/merged-" + id_generator + ".pdf")
+            
+            # Make public and get link
+            blob.make_public()
+            
+            # Delete all processing file
+            for x in file:
+                os.remove(file[x].filename)
+            os.remove("result/merged-" + id_generator + ".pdf")
+            
+            return redirect(blob.public_url)
+        else:
+            return json.dumps(
+                {
+                    "status": "Error (File)",
+                    "description": "Please add the file"
+                }
+                )
         
 # APP Run
 if __name__ == "__main__":
